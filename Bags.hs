3c7d782dc22c74b440b95900c6d50a775fd3754f
Matt Sykes <mtsykes1@sheffield.ac.uk>
2018-10-07 21:55:33 +0100

    Added two new functions.

--- Bags.hs
+++ Bags.hs
@@ -4,7 +4,27 @@ module Bags where
   
   -- listToBag Function
   listToBag :: [String] -> Bag
-  listToBag list
+  listToBag (x:xs)
     | null list = [] -- empty list as input
-    | (head list /= head (tail list)) = (: (head list,length list))
-    | (head list == head (tail list)) = listToBag (tail list)
+    | length xs == 1 = []
+    | x == head xs = listToBag xs
+    | otherwise = []
+    where list = (x:xs)
+            
+  -- bagEqual Function
+  bagEqual :: Bag -> Bag -> Bool
+  bagEqual (x:xs) (y:ys)
+    | length bag1 /= length bag2 = False -- If not equal length than not equal
+    | (null bag1 || null bag2) = False -- Empty bag
+    | ((length bag1) == 1) && ((length bag2) == 1) = (x == y) -- On last item check match, base case
+    | otherwise = x == y && bagEqual xs ys -- If list elements equal next elements, then lists equal
+    where bag1 = (x:xs)
+          bag2 = (y:ys)
+            
+  -- bagSum Function
+  bagSum :: Bag -> Int
+  bagSum (x:xs) -- Split the list into head and tail
+    | length bag == 1 = snd x -- return second part of tuple, base case
+    | length bag >= 1 = snd x + bagSum xs -- add second part of tuple to recursive call result
+    | otherwise = 0
+    where bag = (x:xs)
